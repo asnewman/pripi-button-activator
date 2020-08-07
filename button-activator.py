fedeb7a3646d60846a3c53746c98a4e8575810e1
asnewman <ashznewman@gmail.com>
2020-08-06 22:57:58 -0700

    Use with button

--- button-activator.py
+++ button-activator.py
@@ -7,11 +7,12 @@ GPIO.setup(16, GPIO.IN)
 
 def main():
 	signal.signal(signal.SIGALRM, handler)
-	# while True:
-	# 	if GPIO.input(16) == 0:
-	print('button pressed')
-	signal.alarm(5)
-	request_camera()
+	while True:
+		if GPIO.input(16) == 1:
+			print('button pressed')
+			signal.alarm(5)
+			request_camera()
+			print('request done')
 
 
 def request_camera():
@@ -19,7 +20,6 @@ def request_camera():
 
 
 def handler(signum, frame):
-	print('request done')
 	main()
 
 
